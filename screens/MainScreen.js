16ff896abc9bb91b553b1078721c494808d63e2c
Kimssmin <chakane5@naver.com>
2023-07-30 22:15:15 +0900

    Chapter 5 completed

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,100 +1,114 @@
-import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Text, View, Button} from 'react-native';
+import React, {useCallback} from 'react';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Text, View, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
-function HomeScreen({navigation}) {
-  return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        title="Detail 1 열기"
-        onPress={() =>
-          navigation.push('Detail', {
-            id: 1,
-          })
-        }
-      />
-    </View>
-  );
+function OpenDetailButton() {
+    const navigation = useNavigation();
+
+    return (
+        <Button
+            title="Detail 1 열기"
+            onPress={() => navigation.push('Detail', { id: 1 })}
+        />
+    );
 }
 
+function HomeScreen() {
+    useFocusEffect(
+      useCallback(() => {
+        console.log('이 화면을 보고 있어요.');
+        return () => {
+          console.log('다른 화면으로 넘어갔어요.');
+        };
+      }, []),
+    );
+  
+    return (
+      <View>
+        <Text>Home</Text>
+        <OpenDetailButton />
+      </View>
+    );
+  }
+
 function SearchScreen() {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Text>Search</Text>
+        </View>
+    );
 }
 
 function NotificationScreen() {
-  return (
-    <View>
-      <Text>Notification</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Text>Notification</Text>
+        </View>
+    );
 }
 
 function MessageScreen() {
-  return (
-    <View>
-      <Text>Message</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Text>Message</Text>
+        </View>
+    );
 }
 
 function MainScreen() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        tabBarActiveTintColor: '#fb8c00',
-        tabBarShowLabel: false,
-      }}>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: '홈',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Search"
-        component={SearchScreen}
-        options={{
-          title: '검색',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="search" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notification"
-        component={NotificationScreen}
-        options={{
-          title: '알림',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="notifications" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Message"
-        component={MessageScreen}
-        options={{
-          title: '메시지',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="message" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                tabBarIndicatorStyle: {
+                    backgroundColor: '#009688',
+                },
+                tabBarActiveTintColor: '#009688',
+            }}>
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: '홈',
+                    tabBarIcon: ({ color }) => <Icon name="home" color={color} size={24} />,
+                }}
+            />
+            <Tab.Screen
+                name="Search"
+                component={SearchScreen}
+                options={{
+                    tabBarLabel: '검색',
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="search" color={color} size={24} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Notification"
+                component={NotificationScreen}
+                options={{
+                    tabBarLabel: '알림',
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="notifications" color={color} size={24} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Message"
+                component={MessageScreen}
+                options={{
+                    tabBarLabel: '메시지',
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="message" color={color} size={24} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
 }
 
 export default MainScreen;
